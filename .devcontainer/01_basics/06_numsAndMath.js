a43baec07e840937ff65bdebf4de875e2b47e3ca
Atishay94 <125076899+Atishay94@users.noreply.github.com>
2023-10-28 11:42:09 +0000

    datatypes , Numbers and Math

---
+++ .devcontainer/01_basics/06_numsAndMath.js
@@ -0,0 +1,38 @@
+// Numbers
+const score= 200
+// console.log(score);
+
+const balance= new Number(200)
+// console.log(balance);
+
+const bal1=new Number(score)
+// console.log(bal1);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber=11242.8963
+// console.log(otherNumber.toPrecision(3));
+
+const inNumber= 1000000
+// console.log(inNumber.toLocaleString('en-IN'));
+
+// -----------------++++++++++++++++++++++++------------------
+                    // Maths 
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.sqrt(4));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
